9e33a431fd42c7ff041b9adc971f1a980ffe6d41
No Hit <140995035+Mangle11@users.noreply.github.com>
2026-02-04 14:16:12 -0800

    fix format

--- server/internal/server/write_game.go
+++ server/internal/server/write_game.go
@@ -327,7 +327,7 @@ type Field struct {
 func isRankedByScore(questRun model.QuestRun) bool {
 	if questRun.QuestName == "Endless: Episode 1" || "Endless: Episode 2" {
 		return true
-	} else: {
+	} else {
 		return false
 	}
 }
